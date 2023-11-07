79a2b0f636c4269c1ecd93f2122b8656dbd1a7bc
Divy Gobiraj <divygobi@gmail.com>
2023-11-06 21:38:17 -0800

    made a skeleton

--- listmonad/src/main/java/urmomshouse/App.java
+++ listmonad/src/main/java/urmomshouse/App.java
@@ -1,13 +1,22 @@
 package urmomshouse;
+import java.util.List;
+import java.util.ArrayList;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        getEvens(list);
+    }
+
+    public static void getEvens(List<Integer> list) {
+        ListMonad<Integer> listMonad = new ListMonad<>(list, x -> x.filter(y -> y % 2 == 0));
+        listMonad.printList();
     }
 }
+ 
---
+++ listmonad/src/main/java/urmomshouse/ListMonad.java
@@ -0,0 +1,37 @@
+package main.java.urmomshouse;
+import java.util.List;
+import java.util.function.Function;
+
+public class ListMonad{
+    List<T> list;
+    Function<List<T>, List<T>> f;
+
+    public ListMonad(List<T> list, Function<List<T>, List<T>> f){
+        this.list = list;
+        this.f = f;
+    }
+
+    public void setList(List<T> list){
+        this.list = list;
+    }
+
+    public List<T> getList(){
+        return this.list;
+    }
+
+    public void setFunction(Function<List<T>, List<T>> f){
+        this.f = f;
+    }  
+
+    public Function<List<T>, List<T>> getFunction(){
+        return this.f;
+    }
+
+    public ListMonad<T> bind(Function<List<T>, List<T>> f){
+        return new ListMonad<T>(f.apply(this.list), f);
+    }
+
+    public void printList(){
+        System.out.println(this.list);
+    }
+}
