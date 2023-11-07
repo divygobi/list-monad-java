8ac5262ea760c65b92bad180086428d0f28fef61
Divy Gobiraj <divygobi@gmail.com>
2023-11-06 20:41:14 -0800

    maven setup

---
+++ listmonad/src/main/java/urmomshouse/App.java
@@ -0,0 +1,13 @@
+package urmomshouse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
